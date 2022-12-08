6926d02824e7fc7d8fd22a0790dc98ac1c6fc53d
Win10-SHAYNE\Shayne <shaynec.0205@gamil.com>
2022-12-08 21:13:24 +0800

    feat: i18n not done yet

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import i18n from "./i18n";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(i18n).use(store).use(router).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableLegacy: false
+    }
+  }
 });
