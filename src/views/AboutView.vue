fd55ebf0e4917628fc00969d3ed006a3b2679ab1
Shayne Chang <shayne.chang@zencher.com>
2022-12-12 16:05:51 +0800

    fix: wrap page in a padding container to avoid nav-bar color error

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
-#about.p-3.container
-  .row.py-5
-    .col.text-start
-      h1 About Me
-      h3 Shayne Chang / 張雅琄
-      p a front-end developer based in Taipei, Taiwan.
-      p 目前任職軟體開發工程師近兩年，網頁、手機APP、資料處理都有接觸。
-      p 能使用Vue.js, Flutter, Swift, PySpark, SQL等相關技術，有專案管理經驗。
-      p 閒暇時喜歡旅行、喜歡攝影、喜歡宅在家裡和貓大眼瞪小眼。
-      p 曾任職於醫療、金融、餐飲業、自行創業經營。樂於學習新知、接受挑戰。
-    .col
+.wrapContainer
+  #about.p-3.container
+    .row.py-5
+      .col-12.col-md-6.text-start
+        h1 About Me
+        h3 Shayne Chang / 張雅琄
+        p a front-end developer based in Taipei, Taiwan.
+        p 目前任職軟體開發工程師近兩年，網頁、手機APP、資料處理都有接觸。
+        p 能使用Vue.js, Flutter, Swift, PySpark, SQL等相關技術，有專案管理經驗。
+        p 閒暇時喜歡旅行、喜歡攝影、喜歡宅在家裡和貓大眼瞪小眼。
+        p 曾任職於醫療、金融、餐飲業、自行創業經營。樂於學習新知、接受挑戰。
+      .col-md-6
 </template>
 <style lang="sass" scoped>
 p
   margin: 0
   color: #666
+.wrapContainer
+  padding: 40px
 </style>
