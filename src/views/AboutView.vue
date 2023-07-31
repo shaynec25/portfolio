97b579303aae3904e8fbbb5d82cbfe6b3de82479
Win10-SHAYNE\Shayne <shaynec.0205@gamil.com>
2023-07-31 21:17:01 +0800

    fix: email

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,7 +13,7 @@
         div
           svg(style='width:24px;height:24px' viewbox='0 0 24 24')
             path(fill='currentColor' d='M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z')
-          |  shayne.c0205@gmail.com
+          |  shaynec.0205@gmail.com
       .col-md-6
 </template>
 <script lang="ts">
